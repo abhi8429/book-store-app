feab52780b34f7559f36bb6e5dec984228ac8cdf
abhishek <aksinghabhishek3155@gmail.com>
2024-06-17 22:41:48 +0530

    added auth moddule child routing

--- bookStoreApp/src/app/app-routing.module.ts
+++ bookStoreApp/src/app/app-routing.module.ts
@@ -1,13 +1,21 @@
-import { NgModule } from '@angular/core';
+import { ChangeDetectionStrategy, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { Router,Routes } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { HowItWorksComponent } from './how-it-works/how-it-works.component';
 import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './auth/components/login/login.component';
+import { SignupComponent } from './auth/components/signup/signup.component';
+import { ChangePasswordComponent } from './auth/components/change-password/change-password.component';
 const routes:Routes=[
   {path:'about-us',component:AboutUsComponent},
   {path:'how-it-works',component:HowItWorksComponent},
+  {path:'auth',component:AuthComponent, children:[
+    {path:'login', component:LoginComponent},
+    {path:'signup',component:SignupComponent},
+    {path:'change-password', component:ChangePasswordComponent}
+  ]}
 
 ];
 @NgModule({
